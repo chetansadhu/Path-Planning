51678652f0eecb723d9d14558075a45dd266ae16
Chetan Sadhu <chetansadhu91@gmail.com>
2018-11-26 19:22:20 -0500

    modified main.cpp to take highway map from correct folder

--- src/main.cpp
+++ src/main.cpp
@@ -294,7 +294,7 @@ int main() {
   vector<double> map_waypoints_dy;
 
   // Waypoint map to read from
-  string map_file_ = "data/highway_map.csv";
+  string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
   
 
